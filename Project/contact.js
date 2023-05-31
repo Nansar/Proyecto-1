706142c0205539bc2043409b6d81eea98833a774
Nansar <haptel-sarah@outlook.com>
2023-06-01 00:00:33 +0200

    add projects file

--- Project/contact.js
+++ Project/contact.js
@@ -1,15 +1,17 @@
+
+
 document.getElementById("myForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
 
     // Retrieve form input values
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone-number").value;
+    var message = document.getElementById("text").value;
+
 
     // Do something with the retrieved data
-    console.log("Name: " + name);
-    console.log("Email: " + email);
-
-    // You can perform further operations or send the data to a server
+    console.log("Thank you! Your message has been sent!");
 });
 
 
---
+++ Project/home.js
@@ -0,0 +1,14 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopButton").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopButton").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
