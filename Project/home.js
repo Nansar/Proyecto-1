7faada587468a8d1043960b48d44d0a677822746
Nansar <haptel-sarah@outlook.com>
2023-06-02 02:14:50 +0200

    Proyecto 1

--- Project/home.js
+++ Project/home.js
@@ -1,14 +1,46 @@
-window.onscroll = function () { scrollFunction() };
+document.addEventListener('DOMContentLoaded', function () {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scrollToTopButton").style.display = "block";
-    } else {
-        document.getElementById("scrollToTopButton").style.display = "none";
+    // brands parade
+
+    function scrollImages() {
+        const container = document.getElementById('brands-row');
+        const imgCols = container.getElementsByClassName('img-col');
+        const images = Array.from(imgCols).map((imgCol) => imgCol.firstElementChild);
+
+        let scrollPosition = container.scrollLeft;
+        const containerWidth = container.offsetWidth;
+        const totalWidth = container.scrollWidth;
+
+        function loop() {
+            scrollPosition += 1;
+            if (scrollPosition >= totalWidth - containerWidth) {
+                scrollPosition = 0;
+            }
+            container.scrollLeft = scrollPosition;
+            requestAnimationFrame(loop);
+        }
+
+        loop();
     }
-}
 
-function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+    scrollImages();
+
+
+    // scroll to top button
+
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollToTopButton").style.display = "block";
+        } else {
+            document.getElementById("scrollToTopButton").style.display = "none";
+        }
+    }
+
+    function scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+});
